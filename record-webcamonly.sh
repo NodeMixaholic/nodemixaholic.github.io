95c4fc10f634fa254bdbb9d20724865fd5d65e39
Samuel Lord <sam@sllord.info>
2022-02-25 22:04:32 -0600

    Shrink file size.
    Not to be confused with the record.sh compression, this just scales the camera down.

--- record-webcamonly.sh
+++ record-webcamonly.sh
@@ -4,7 +4,7 @@ audioout="alsa_output.usb-Logitech_USB_Headset_Logitech_USB_Headset-00.analog-st
 audioin="alsa_input.usb-Logitech_USB_Headset_Logitech_USB_Headset-00.mono-fallback"
 
 
-ffmpeg -f pulse -i $audioin -ac 1 -f mp3 - | ffplay - |  ffmpeg -f v4l2 -thread_queue_size 64 -video_size 640x360 -framerate 30 -i /dev/video0 \
+ffmpeg -f pulse -i $audioin -ac 1 -f mp3 - | ffplay - |  ffmpeg -f v4l2 -thread_queue_size 64 -video_size 400x225 -framerate 30 -i /dev/video0 \
        -f pulse -i $audioout -ac 2 \
        -vcodec libx264 -preset ultrafast -qp 0 \
-       "bloatless-videos/$1.mp4"
+       "bloatless-videos/$1.smallfinal.mp4"
